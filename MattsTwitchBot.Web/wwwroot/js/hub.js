b2d4a1c4e5fb7179e111f2e3d381376ab36e2fd1
Matthew D. Groves <me@mgroves.com>
2019-11-05 09:02:23 -0500

    tweak to console message

--- MattsTwitchBot.Web/wwwroot/js/hub.js
+++ MattsTwitchBot.Web/wwwroot/js/hub.js
@@ -11,7 +11,7 @@ window.onload = function () {
     connection.on("ReceiveSoundEffect", HandleSoundEffect);
     connection.on("ReceiveFanfare", HandleFanfare);
     connection.start().then(function () {
-        console.log("Hello, Twitch. My Chat Bot is ready!");
+        console.log("Hello, console. My Chat Bot is ready!");
         var myNotus = notus();
         myNotus.send({
             notusType: 'toast',
