7c3b764209e5f05ee357c2f50634d3b0c9e85bc3
Matthew D. Groves <me@mgroves.com>
2020-04-02 16:39:04 -0400

    added !trout command

---
+++ MattsTwitchBot.Core/Requests/Trout.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+using Couchbase.Annotations;
+using MediatR;
+using TwitchLib.Client.Models;
+
+namespace MattsTwitchBot.Core.Requests
+{
+    public class Trout : IRequest
+    {
+        public string UserToTrout { get; }
+        public string Channel { get; }
+
+        public Trout(ChatMessage chatMessage)
+        {
+            UserToTrout = chatMessage.Message.Replace("!trout ", "");
+            Channel = chatMessage.Channel;
+        }
+    }
+}
