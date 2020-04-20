ed8a299bc6bc76a274a578e3e0c2a20f3787814a
Matthew D. Groves <me@mgroves.com>
2020-04-20 11:53:27 -0400

    added !lurk command, introduced scriban template enging

---
+++ MattsTwitchBot.Core/Requests/Lurk.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TwitchLib.Client.Models;
+
+namespace MattsTwitchBot.Core.Requests
+{
+    public class Lurk : IRequest
+    {
+        public ChatMessage ChatMessage { get; }
+
+        public Lurk(ChatMessage chatMessage)
+        {
+            ChatMessage = chatMessage;
+        }
+    }
+}
---
+++ MattsTwitchBot.Tests/Core/RequestHandlerTests/LurkHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MattsTwitchBot.Core.RequestHandlers;
+using MattsTwitchBot.Core.Requests;
+using Moq;
+using NUnit.Framework;
+using TwitchLib.Client.Interfaces;
+using TwitchLib.Client.Models.Builders;
+
+namespace MattsTwitchBot.Tests.Core.RequestHandlerTests
+{
+    [TestFixture]
+    public class LurkHandlerTests
+    {
+        private LurkHandler _handler;
+        private Mock<ITwitchClient> _mockTwitchClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockTwitchClient = new Mock<ITwitchClient>();
+            _handler = new LurkHandler(_mockTwitchClient.Object);
+        }
+
+        [Test]
+        public async Task A_random_lurk_message_is_sent_with_the_users_name()
+        {
+            // arrange
+            var expectedUsername = "whatevername" + Guid.NewGuid();
+            var twitchLibMessage = TwitchLibMessageBuilder.Create()
+                .WithUsername(expectedUsername)
+                .Build();
+            var chatMessage = ChatMessageBuilder.Create()
+                .WithTwitchLibMessage(twitchLibMessage)
+                .WithMessage("!help")
+                .Build();
+            var req = new Lurk(chatMessage);
+
+            // act
+            await _handler.Handle(req, CancellationToken.None);
+
+            // assert
+            _mockTwitchClient.Verify(m => m.SendMessage(It.IsAny<string>(),
+                It.Is<string>(s => s.Contains(expectedUsername)), false),Times.Once);
+        }
+    }
+}
